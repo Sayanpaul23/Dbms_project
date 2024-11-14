ce4fe31e7afe5d1f92a663ae889cb90d8b5df04b
Sayanpaul23 <sayanpaulcollage@gmail.com>
2024-11-14 20:37:47 +0530

    Update db.sql

--- DBMS_Project/DB/db.sql
+++ DBMS_Project/DB/db.sql
@@ -3,22 +3,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dailyexpense`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `expenses`
---
-
 CREATE TABLE `expenses` (
   `expense_id` int(20) NOT NULL,
   `user_id` varchar(15) NOT NULL,
